26ae627a99464b7745e46120e31edd4dddb5da2a
Kyng Murray <jordan.murray@codeimmersives.com>
2022-02-15 17:39:51 -0800

    Completed Hello-Node

---
+++ main.js
@@ -0,0 +1,11 @@
+console.log("Hello World ")
+
+console.log("HELLO, WORLd")
+console.log("\n")
+console.log("Goodbye world!")
+
+console.log("HH   HH           LLL       LLL                                                          LLL        DDDDDDD")
+console.log("HH   HH  EEEEEEE  LLL       LLL       OOOOOOOO  WW     WWW     WW  OOOOOOOO  RRR RRRRR   LLL        DD   DDDD")
+console.log("HHHHHHH  EE  EEE  LLL       LLL       OOO  OOO   WW    WWW    WW   OOO  OOO  RRR  RRRR   LLL        DD    DDDDD")
+console.log("HH   HH  EE       LLLLLLLL  LLLLLLLL  OOO  OOO    WW  WW WW  WW    OOO  OOO  RR  RRR     LLLLLLLL   DD   DDDD")
+console.log("HH   HH  EEEEEEE  LLLLLLLL  LLLLLLLL  OOOOOOOO     WWWW   WWWW     OOOOOOOO  RR    RRRR  LLLLLLLL   DDDDDDDD")
